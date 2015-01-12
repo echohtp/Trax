a68a60ca9fdc86fb6a73ff486d09c7c5fcb50f55
Jason Schorr <echohtp@gmail.com>
2015-01-12 11:55:52 -0800

    thing

--- js/site/reddit.js
+++
@@ -1,8 +0,0 @@
-var base_url = 'http://reddit.com/';
-var subreddit = $(location).attr('pathname');
-
-$.getJSON(base_url + subreddit, function(data) {
-    console.log('Recieved data from Reddit.');
-    var subData = jQuery.parseJSON(data);
-
-});
